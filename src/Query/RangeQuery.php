354ddbf9bac06f77ab423a27da1caaa83a2a2fd8
Olzhas Kulzhambekov <exgamer@live.ru>
2021-08-24 15:07:06 +0600

    +

---
+++ src/Query/RangeQuery.php
@@ -0,0 +1,81 @@
+<?php
+namespace Citizenzet\ElasticQueryBuilder\Query;
+
+use Citizenzet\ElasticQueryBuilder\Exceptions\QueryException;
+
+/**
+ * Class RangeQuery
+ * @package Citizenzet\ElasticQueryBuilder\Query
+ * @author Olzhas Kulzhambekov <exgamer@live.ru>
+ */
+class RangeQuery extends Query
+{
+    protected $field;
+    protected $lt;
+    protected $gt;
+    protected $lte;
+    protected $gte;
+
+    public function setField(string $field)
+    {
+        $this->field = $field;
+
+        return $this;
+    }
+
+    public function gt($value)
+    {
+        $this->gt = $value;
+
+        return $this;
+    }
+
+    public function lt($value)
+    {
+        $this->lt = $value;
+
+        return $this;
+    }
+
+    public function gte($value)
+    {
+        $this->gte = $value;
+
+        return $this;
+    }
+
+    public function lte($value)
+    {
+        $this->lte = $value;
+
+        return $this;
+    }
+
+    public function build(): array
+    {
+        $query = [];
+
+        if (null !== $this->gt) {
+            $query['gt'] = $this->gt;
+        }
+        if (null !== $this->lt) {
+            $query['lt'] = $this->lt;
+        }
+        if (null !== $this->gte) {
+            $query['gte'] = $this->gte;
+        }
+        if (null !== $this->lte) {
+            $query['lte'] = $this->lte;
+        }
+
+        if (empty($query)) {
+            throw new QueryException('Empty Query');
+        }
+
+        return [
+            'range' => [
+                $this->field => $query,
+            ],
+        ];
+    }
+}
--- src/Query/TermQuery.php
+++ src/Query/TermQuery.php
@@ -1,4 +1,3 @@
-
 <?php
 namespace Citizenzet\ElasticQueryBuilder\Query;
 
